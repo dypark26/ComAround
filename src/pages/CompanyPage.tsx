1663d4f5cb45f7c810e18a0d4daad8ef922289f3
Dayoung Park <qkrekdud26@gmail.com>
2023-01-21 15:47:43 +0900

    :sparkles:feat 임시 세팅 #1 #6 모달창 만들기 전 임시 페이지 생성 및 라우터연결했습니다. by @dypark26

--- src/pages/CompanyPage.tsx
+++ src/pages/CompanyPage.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import styled from 'styled-components';
 
 const CompanyPage = () => {
-  return <div>CompanyPage</div>;
+  return (
+    <CompanyPageContainer>
+      <SideNav>SideNav</SideNav>
+      <MainArea>
+        <Map>Map</Map>
+        <CompanyReview>CompanyReview</CompanyReview>
+      </MainArea>
+    </CompanyPageContainer>
+  );
 };
 
 export default CompanyPage;
+
+const CompanyPageContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const SideNav = styled.div`
+  width: 300px;
+  height: 100vh;
+  background-color: aqua;
+`;
+const MainArea = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  width: calc(100% - 300px);
+  height: 100vh;
+`;
+const Map = styled.div`
+  height: 50vh;
+  background-color: beige;
+`;
+const CompanyReview = styled.div`
+  height: 50vh;
+  background-color: tomato;
+`;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MainPage = () => {
-  return <div>MainPage</div>;
+  return (
+    <div>
+      <Link to={`/company`}>company</Link>
+    </div>
+  );
 };
 
 export default MainPage;
