84153d7d1b3dd3cd7f4430b80987f781b259bb93
ddoqi <bongbk72@naver.com>
2023-01-26 17:15:51 +0900

    🔀 feat: detail-page브랜치 합병 및 데이터 계산 로직 추가 #1 #4  MainPage, Modal, CompanyPage, Map 합병  데이터 계산 로직 추가 ( 평균값, 총개수 )
    by @ddoqi

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import Router from './shared/Router';
+import Router from "./shared/Router";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 function App() {
-  return <Router />
-  ;
+  // 리액트쿼리 추가 사용하여, 해당 페이지에 queryClient를 추가하였습니다.
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
